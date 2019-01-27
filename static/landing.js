2b667c3ce9656074af104c7e765cad6a92aceb6d
Connor13C <connorbcarey@hotmail.com>
2019-01-27 12:25:11 -0700

    added more to UI

--- static/landing.js
+++ static/landing.js
@@ -11,13 +11,24 @@ $('#login').click(function () {
         contentType: 'application/json',
         success: function (data, textStatus, jQxhr) {
 		access_token = JSON.stringify(data);
-		console.log(access_token)
-        $('#login_field').hide()
-        $('#post_request').show()
+		console.log(access_token);
+        $('#login_field').hide();
+        $('#post_request').show();
+        $('#choice').show();
 	    },
 	    error: function (jqXhr, textStatus, errorThrown) {
 		console.log(errorThrown);
 	    }
     });
     return false;
+});
+
+$('#get_r').click(function () {
+    $('form').hide();
+    $('#get_request').show();
+});
+
+$('#post_r').click(function () {
+    $('form').hide();
+    $('#post_request').show();
 });
